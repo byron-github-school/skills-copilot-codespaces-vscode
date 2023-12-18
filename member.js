f62d0bbc15d5af7f6e037d50ea78e679db643f30
Byron Goodman2 <byron.goodman@infomagnus.com>
2023-12-18 15:20:17 -0800

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skilsMember() {
+  var member = document.getElementById("member");
+  var memberValue = member.value;
+  var memberResult = document.getElementById("memberResult");
+  memberResult.innerHTML = memberValue;
+}
